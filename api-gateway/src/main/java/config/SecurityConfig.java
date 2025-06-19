184a51024b5c8d37f7f4131e908674c29c9a299e
công lê xuân <congle.190904@gmail.com>
2025-06-19 21:29:03 +0700

    config logout security

--- api-gateway/src/main/java/config/SecurityConfig.java
+++ api-gateway/src/main/java/config/SecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
@@ -12,6 +17,13 @@ import org.springframework.security.web.server.authentication.logout.ServerLogou
 @EnableWebFluxSecurity
 // config theo Reactive application (webFlux)
 public class SecurityConfig {
+    private final ReactiveClientRegistrationRepository clientRegistrationRepository;
+
+    // bean sẽ được inject từ file cấu hinhf
+    public SecurityConfig(ReactiveClientRegistrationRepository clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
+
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         return  http
@@ -23,14 +35,23 @@ public class SecurityConfig {
                 .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .logout(logout -> logout
-                        .logoutSuccessHandler(null)
+                        .logoutSuccessHandler(this.serverLogoutSuccessHandler())
                 )
                 .build();
     }
-    private ServerLogoutSuccessHandler serverLogoutSuccessHandler() {
 
+    private ServerLogoutSuccessHandler serverLogoutSuccessHandler() {
+        OidcClientInitiatedServerLogoutSuccessHandler oidcClientInitiatedServerLogoutSuccessHandler =
+                new OidcClientInitiatedServerLogoutSuccessHandler(this.clientRegistrationRepository);
+        oidcClientInitiatedServerLogoutSuccessHandler.setPostLogoutRedirectUri("http://localhost:3000");
+        return oidcClientInitiatedServerLogoutSuccessHandler;
     }
 
+    // mapper sang quyền từ oauth2 loggin()
+    @Bean
+    public GrantedAuthoritiesMapper grantedAuthoritiesMapper(){
+
+    }
 
 
 
