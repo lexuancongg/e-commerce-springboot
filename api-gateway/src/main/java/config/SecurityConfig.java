708ac54d32e56c37566562c5813c6b0f3fde7e71
công lê xuân <congle.190904@gmail.com>
2025-06-20 22:36:03 +0700

    config authority for security config

--- api-gateway/src/main/java/config/SecurityConfig.java
+++ api-gateway/src/main/java/config/SecurityConfig.java
@@ -5,19 +5,28 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebFluxSecurity
 // config theo Reactive application (webFlux)
 public class SecurityConfig {
     private final ReactiveClientRegistrationRepository clientRegistrationRepository;
+    private  final String REALM_ACCESS_CLAIM = "realm_access";
+    private  final String ROLES_CLAIM = "roles";
 
     // bean sẽ được inject từ file cấu hinhf
     public SecurityConfig(ReactiveClientRegistrationRepository clientRegistrationRepository) {
@@ -50,9 +59,37 @@ public class SecurityConfig {
     // mapper sang quyền từ oauth2 loggin()
     @Bean
     public GrantedAuthoritiesMapper grantedAuthoritiesMapper(){
+        return  (authorities)->{
+            Set<GrantedAuthority> grantedAuthoritySet = new HashSet<>();
+            GrantedAuthority authority = authorities.iterator().next();
+            boolean isOidc = authority instanceof OidcUserAuthority;
+            if(isOidc){
 
+                OidcUserAuthority oidcUserAuthority = (OidcUserAuthority)authority;
+                OidcUserInfo oidcUserInfo = oidcUserAuthority.getUserInfo();
+                if(oidcUserInfo.hasClaim(this.REALM_ACCESS_CLAIM)){
+                    Map<String, Object> realmAccessClaim = oidcUserInfo.getClaimAsMap(this.REALM_ACCESS_CLAIM);
+                    Collection<String> roles = (Collection<String>) realmAccessClaim.get(this.ROLES_CLAIM);
+                    grantedAuthoritySet.addAll(this.buildAuthoritiesFromClaimRoles(roles));
+                }
+                // nếu không phải OIDC
+            }else{
+                OAuth2UserAuthority oAuth2UserAuthority = (OAuth2UserAuthority)authority;
+                Map<String ,Object> userAttributes = oAuth2UserAuthority.getAttributes();
+                if(userAttributes.containsKey(this.REALM_ACCESS_CLAIM)){
+                    Map<String,Object> realmAccessClaim = (Map<String, Object>) userAttributes.get(this.REALM_ACCESS_CLAIM);
+                    Collection<String> roles = (Collection<String>) realmAccessClaim.get(this.ROLES_CLAIM);
+                    grantedAuthoritySet.addAll(this.buildAuthoritiesFromClaimRoles(roles));
+                }
+            }
+            return grantedAuthoritySet;
+        };
     }
-
+    private Collection<GrantedAuthority> buildAuthoritiesFromClaimRoles(Collection<String> roles){
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_".concat(role)))
+                .collect(Collectors.toList());
+    }
 
 
 }
@@ -60,3 +97,4 @@ public class SecurityConfig {
 
 // doc:
 //https://docs.spring.io/spring-security/reference/reactive/authentication/x509.html
+// nếu oidc thì thông tin đuược mapper vào OidcUserAuthority , còn không thì OAuth2UserAuthority
