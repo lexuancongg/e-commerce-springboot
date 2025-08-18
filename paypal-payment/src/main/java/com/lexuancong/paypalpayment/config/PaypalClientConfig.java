f89a54eb2cd6f84ae4478b01e5ce33fcc78bdecd
công lê xuân <congle.190904@gmail.com>
2025-08-18 14:00:56 +0700

    code create order of paypal proment

--- paypal-payment/src/main/java/com/lexuancong/paypalpayment/config/PaypalClientConfig.java
+++ paypal-payment/src/main/java/com/lexuancong/paypalpayment/config/PaypalClientConfig.java
@@ -1,11 +1,15 @@
 package com.lexuancong.paypalpayment.config;
 
 
-
 import com.paypal.core.PayPalEnvironment;
 import com.paypal.core.PayPalHttpClient;
+import com.paypal.sdk.Environment;
+import com.paypal.sdk.PaypalServerSdkClient;
+import com.paypal.sdk.authentication.ClientCredentialsAuthModel;
+import com.paypal.sdk.models.OAuthToken;
 import lombok.Getter;
 import lombok.Setter;
+import org.slf4j.event.Level;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -22,16 +26,42 @@ public class PaypalClientConfig {
     @Value("${paypal.mode}")
     private String mode;
 
-    // bean đại diện cho client khi thao tác với sdk paypal nhu gọi api
+    // bean đại diện cho client khi thao tác với sdk paypal nhu gọi api cho kiểu cũ
     @Bean
     public PayPalHttpClient payPalHttpClient() {
-         if(this.mode.equals("sanbox")){
-             return new PayPalHttpClient( new PayPalEnvironment.Sandbox(this.clientId, this.clientSecret));
-         }
-         return new PayPalHttpClient(new PayPalEnvironment.Live(this.clientId, this.clientSecret));
+        if (this.mode.equals("sanbox")) {
+            return new PayPalHttpClient(new PayPalEnvironment.Sandbox(this.clientId, this.clientSecret));
+        }
+        return new PayPalHttpClient(new PayPalEnvironment.Live(this.clientId, this.clientSecret));
     }
 
 
+    // sdk kiểu mới của sdk => mới phát triển
+
+    @Bean
+    public PaypalServerSdkClient paypalClient() {
+        return new PaypalServerSdkClient.Builder()
+                .loggingConfig(builder -> builder
+                        .level(Level.DEBUG)
+                        .requestConfig(logConfigBuilder -> logConfigBuilder.body(true))
+                        .responseConfig(logConfigBuilder -> logConfigBuilder.headers(true)))
+                .httpClientConfig(configBuilder -> configBuilder
+                        .timeout(0))
+                .environment(Environment.SANDBOX)
+                .clientCredentialsAuth(new ClientCredentialsAuthModel.Builder(this.clientId, this.clientSecret)
+                                .oAuthTokenProvider((lastOAuthToken, credentialsManager) -> {
+                                    // Add the callback handler to provide a new OAuth token
+                                    // It will be triggered whenever the lastOAuthToken is undefined or expired
+                                    OAuthToken oAuthToken = loadTokenFromDatabase();
+                                    if (oAuthToken != null && !credentialsManager.isTokenExpired(oAuthToken)) {
+                                        return oAuthToken;
+                                    }
+                                    return credentialsManager.fetchToken();
+                                })
+                                .build()
+                )
+                .build();
+    }
 
 
 }
--- paypal-payment/src/main/java/com/lexuancong/paypalpayment/service/PaypalService.java
+++ paypal-payment/src/main/java/com/lexuancong/paypalpayment/service/PaypalService.java
@@ -3,17 +3,54 @@ package com.lexuancong.paypalpayment.service;
 import com.lexuancong.paypalpayment.viewmodel.PaypalCreatePaymentRequest;
 import com.lexuancong.paypalpayment.viewmodel.PaypalCreatePaymentResponse;
 import com.paypal.core.PayPalHttpClient;
+import com.paypal.orders.AmountWithBreakdown;
 import com.paypal.orders.OrderRequest;
+import com.paypal.orders.PurchaseUnitRequest;
+import com.paypal.sdk.PaypalServerSdkClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PaypalService {
     private final PayPalHttpClient payPalHttpClient;
+    private final PaypalServerSdkClient paypalServerSdkClient;
+
+    private final BigDecimal maxPay = new BigDecimal(100);
 
     public PaypalCreatePaymentResponse createPayment(PaypalCreatePaymentRequest paypalCreatePaymentRequest) {
+        // đại diện cho data body trong req
         OrderRequest orderRequest = new OrderRequest();
+        orderRequest.checkoutPaymentIntent("CAPTURE");
+        BigDecimal totalPrice = paypalCreatePaymentRequest.totalPrice();
+        // chia transaction ra nếu viwotj quá maxPay
+        List<BigDecimal> transactions = new ArrayList<>();
+        while (totalPrice.compareTo(BigDecimal.ZERO) > 0) {
+            if(totalPrice.compareTo(maxPay) > 0) {
+                totalPrice = totalPrice.subtract(maxPay);
+                transactions.add(maxPay);
+                continue;
+            }
+            transactions.add(totalPrice);
+            totalPrice = BigDecimal.ZERO;
+
+        }
+
+        for (BigDecimal transaction : transactions) {
+            PurchaseUnitRequest purchaseUnitRequest = new PurchaseUnitRequest()
+                    .amountWithBreakdown(
+                            new AmountWithBreakdown()
+                                    .currencyCode("USD")
+                    );
+
+        }
+
+
 
     }
 
